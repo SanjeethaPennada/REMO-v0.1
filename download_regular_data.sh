eca2110ca85037086ca92fc0466983d2f3086798
Sanjeetha Pennada <s.pennada@sheffield.ac.uk>
2025-03-07 13:12:44 +0000

    updated

--- download_regular_data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Download regular data for AIM-BEVs
-mkdir aim_bev_regular_data
-cd aim_bev_regular_data
-wget https://s3.eu-central-1.amazonaws.com/avg-projects/king/king_dataset.zip
-unzip king_dataset.zip
-rm king_dataset.zip
-cd ..
